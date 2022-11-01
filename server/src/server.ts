14b8a791e794a9071e5b7fface9ff2a931f2c3cb
Kelvin Reverts <kelvinrev2@hotmail.com>
2022-10-31 23:13:53 -0300

    Initial commit

---
+++ server/src/server.ts
@@ -0,0 +1,13 @@
+import Fastify from 'fastify'
+
+async function bootstrap(){
+
+    const fastify = Fastify({
+        logger: true,
+    })
+
+    await fastify.listen({ port: 3333})
+
+}
+
+bootstrap()
