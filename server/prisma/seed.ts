9d4ee4b8c8fcc8ff7ad62fde7b54ab6d5beeb51a
Kelvin Reverts <kelvinrev2@hotmail.com>
2022-11-02 16:46:09 -0300

    Create seed complete

---
+++ server/prisma/seed.ts
@@ -0,0 +1,62 @@
+import {PrismaClient} from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+async function main() {
+    const user = await prisma.user.create({
+        data:{
+            name: 'Test data',
+            email: 'test.data@email.com',
+            avatarUrl: 'http://github.com/kelvinrevertis.png'
+        }
+    })
+    const pool = await prisma.pool.create({
+        data:{
+            title: 'Example Pool',
+            code: 'BOL123',
+            ownerId: user.id,
+
+            participants: {
+                create:{
+                    userId: user.id
+                }
+            }
+        }
+    })
+
+    await prisma.game.create({
+        data:{
+            date: '2022-11-02T12:00:00.463Z',
+            firstTeamCountryCode: 'DE',
+            secondTeamCountryCode: 'BR',
+        }
+    })
+    
+    await prisma.game.create({
+        data:{
+            date: '2022-11-02T12:00:00.463Z',
+            firstTeamCountryCode: 'DE',
+            secondTeamCountryCode: 'BR',
+
+            guesses:{
+                create:{
+                    firstTeamPoints: 2,
+                    secondTeamsPoint: 1,
+
+                    participant:{
+                        connect: {
+                            userId_poolId:{
+                                userId: user.id,
+                                poolId: pool.id,
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    })
+}
+
+
+
+main()
