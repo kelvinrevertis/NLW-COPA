157f6079f2d03e927bea2b0c814d68972f54b11f
Kelvin Reverts <kelvinrev2@hotmail.com>
2022-11-01 21:42:29 -0300

    Test NativeBase VStack

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,20 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { NativeBaseProvider, VStack, Center, Text } from "native-base";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js </Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider>
+
+      <VStack flex={1} bgColor="amber.800" alignItems='center' justifyContent="center">
+        <Text>Test Texto</Text>
+        <Text style={styles.title}>Open up App.js to!</Text>
+        <StatusBar style="auto" />
+      </VStack>
+    </NativeBaseProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    color: 'white',
+    fontSize: 24
+  }
 });
