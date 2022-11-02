92d4a9a35026e7e1c3b157b59caea62cbba1eec1
Kelvin Reverts <kelvinrev2@hotmail.com>
2022-11-02 17:08:37 -0300

    Install Zod and Short Unique Id
    use zod to validate schema,
    Use Short Unique id(uuid) to generate a random ID

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,34 +1,59 @@
 import Fastify from 'fastify'
 import cors from '@fastify/cors'
-import {PrismaClient} from '@prisma/client'
+import { z } from 'zod'
+import { PrismaClient } from '@prisma/client'
+import ShortUniqueId from 'short-unique-id'
 
 
 const prisma = new PrismaClient({
     log: ['query']
 })
 
-async function bootstrap(){
+async function bootstrap() {
 
     const fastify = Fastify({
         logger: true,
     })
 
-    await fastify.register(cors,{
-        origin:true,
+    await fastify.register(cors, {
+        origin: true,
     })
 
-    fastify.get('/pools/count', async () =>{
+    fastify.get('/pools/count', async () => {
         const count = await prisma.pool.count()
         // const pools = await prisma.pool.findMany({
         //     where:{
         //         code: {startsWith: 'K'}
         //     }
         // })
-        
-        return { count}
+
+        return { count }
     })
 
-    await fastify.listen({ port: 3333/*, *host: '0.0.0.0'*/})
+    fastify.post('/pools', async (request, reply) => {
+
+        const createPoolBody = z.object({
+            title: z.string(),
+        })
+
+        const { title } = createPoolBody.parse(request.body)
+
+        const generate = new ShortUniqueId({length: 6})
+        const code = String(generate()).toUpperCase()
+
+        await prisma.pool.create({
+            data:{
+                title,
+                code
+            }
+        })
+
+        return reply.status(201).send({ title,code })
+
+
+    })
+
+    await fastify.listen({ port: 3333/*, *host: '0.0.0.0'*/ })
 
 }
 
