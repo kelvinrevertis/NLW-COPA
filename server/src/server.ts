5d91d7ead37c85d6ac04e206582e420c4198f1ae
Kelvin Reverts <kelvinrev2@hotmail.com>
2022-11-02 21:26:55 -0300

    create routes users and guesses

--- server/src/server.ts
+++ server/src/server.ts
@@ -30,6 +30,20 @@ async function bootstrap() {
         return { count }
     })
 
+    fastify.get('/users/count', async () => {
+        const count = await prisma.user.count()
+
+
+        return { count }
+    })
+
+    fastify.get('/guesses/count', async () => {
+        const count = await prisma.guess.count()
+
+
+        return { count }
+    })
+
     fastify.post('/pools', async (request, reply) => {
 
         const createPoolBody = z.object({
