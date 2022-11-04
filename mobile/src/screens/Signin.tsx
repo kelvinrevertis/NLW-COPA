34431e0a38d0b2f0b7d96113dd7f2ab3ed21d0fd
Kelvin Reverts <kelvinrev2@hotmail.com>
2022-11-03 23:51:15 -0300

    Create button
    Props and stylize

--- mobile/src/screens/Signin.tsx
+++ mobile/src/screens/Signin.tsx
@@ -1,11 +1,19 @@
-import {Center, Text } from "native-base"
+import { Center, Text, Icon } from "native-base"
+import {Fontisto} from "@expo/vector-icons"
 
-export function SignIn(){
-    return(
+import Logo from '../assets/logo.svg'
+import { Button } from "../components/Button"
+
+export function SignIn() {
+    return (
         <Center flex={1} bgColor="gray.900">
-            <Text color="white" fontSize={24} fontFamily="heading">
-            Sign In!
-            </Text>
+            <Logo width={212} height={40} />
+
+            <Button 
+            leftIcon={<Icon as={Fontisto} name="google" color="white" size="md"/>} 
+            title="ENTRAR COM O GOOGLE"
+            type="SECONDARY"/>
+
         </Center>
     )
-}
+}           
