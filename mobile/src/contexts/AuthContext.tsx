f5ec57037a5a454461fba414e6e683030f64e308
Kelvin Reverts <kelvinrev2@hotmail.com>
2022-11-05 20:19:44 -0300

    Google authentication

--- mobile/src/contexts/AuthContext.tsx
+++ mobile/src/contexts/AuthContext.tsx
@@ -1,5 +1,7 @@
-import { createContext, ReactNode } from "react";
+import { createContext, ReactNode, useState, useEffect } from "react";
+import * as Google from 'expo-auth-session/providers/google'
 import * as AuthSession from 'expo-auth-session'
+import * as WebBrowser from 'expo-web-browser'
 
 interface UserProps{
     name: string
@@ -8,6 +10,7 @@ interface UserProps{
 
 export interface AuthContextDataProps{
     user: UserProps
+    isUserLoading: boolean
     signIn: ()=> Promise<void>
 }
 
@@ -19,17 +22,44 @@ interface AuthProviderProps{
 export const AuthContext = createContext({} as AuthContextDataProps)
 
 export function AuthContextProvider({children}: AuthProviderProps){
+    const [user, setUser] = useState<UserProps>({} as UserProps)
+    const[isUserLoading, setIsUserLoading] = useState(false)
 
-    console.log(AuthSession.makeRedirectUri({useProxy: true}))
+   const[request, response, promptAsync] =  Google.useAuthRequest({
+        clientId: '214174115282-dfppf2flsclcg5tlftrimm6turtv5e36.apps.googleusercontent.com',
+        redirectUri:AuthSession.makeRedirectUri({useProxy: true}),
+        scopes: ['profile','email']
+    })
+
+    console.log()
 
     async function signIn(){
-        console.log("Vamos Logar!")
+        try{
+            setIsUserLoading(true)
+            await promptAsync()
 
+        }catch(error){
+            console.log(error)
+            throw error
+        }finally{
+            setIsUserLoading(false)
+        }
     }
+
+    async function signiInWithGoogle(access_token : string) {
+        console.log("TOKEN DE AUTENTICAÇÃO ===>", access_token)
+    }
+
+    useEffect(() =>{
+        if(response?.type === 'success' && response.authentication?.accessToken){
+            signiInWithGoogle(response.authentication.accessToken)
+        }
+    },[response])
 
     return(
         <AuthContext.Provider value={{
             signIn,
+            isUserLoading,
             user:{
                 name: 'Kelvin',
                 avatarUrl: 'https://www.github.com/kelvinrevertis.png'
