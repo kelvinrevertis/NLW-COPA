edb9970db76ecc94ac00109637046a44fac83eda
Kelvin Reverts <kelvinrev2@hotmail.com>
2022-11-09 21:47:50 -0300

    Complete Details

--- mobile/src/screens/Details.tsx
+++ mobile/src/screens/Details.tsx
@@ -1,31 +1,37 @@
 import { useEffect, useState } from "react";
-import { useToast, VStack } from "native-base";
+import { HStack, useToast, VStack } from "native-base";
 import { useRoute } from "@react-navigation/native";
 
-import { api } from "../services/api"; 
+import { api } from "../services/api";
 
 import { Header } from "../components/Header";
 import { Loading } from "../components/Loading";
+import { PoolCardProps } from '../components/PoolCard';
+import { PoolHeader } from "../components/PoolHeader";
+import { EmptyMyPoolList } from "../components/EmptyMyPoolList";
+import {Option} from '../components/Option'
 
-interface RouteParams{
+interface RouteParams {
     id: string;
 }
 
 export function Details() {
+    const [optionSelected, setOptionSelected] = useState<'guesses' | 'ranking'>('guesses');
     const [isLoading, setIsLoading] = useState(false);
+    const [poolDetails, setPoolDetails] = useState<PoolCardProps>({} as PoolCardProps);
 
     const route = useRoute();
     const toast = useToast();
-    const {id} = route.params as RouteParams;
+    const { id } = route.params as RouteParams;
 
-    async function fetchPoolDetails(){
-        try{
+    async function fetchPoolDetails() {
+        try {
             setIsLoading(true);
 
-        const response = await api.get(`/pools/${id}`);
-        console.log(response)
+            const response = await api.get(`/pools/${id}`);
+            setPoolDetails(response.data.pool)
 
-        }catch(error){
+        } catch (error) {
             console.log(error);
 
             toast.show({
@@ -33,24 +39,44 @@ export function Details() {
                 placement: 'top',
                 bgColor: 'red.500'
             });
-            
-        }finally{
+
+        } finally {
             setIsLoading(false)
         }
     }
 
-    useEffect(() =>{
+    useEffect(() => {
         fetchPoolDetails();
     }, [id]);
 
-    if(isLoading){
-        return <Loading/>
+    if (isLoading) {
+        return <Loading />
     }
 
     return (
         <VStack flex={1} bgColor="gray.900">
-            <Header title={id} showBackButton showShareButton />
-        
+            <Header title={poolDetails.title} showBackButton showShareButton />
+
+            {
+                poolDetails._count?.participants > 0 ?
+                    <VStack px={5} flex={1}>
+                        <PoolHeader data={poolDetails} />
+
+                        <HStack bgColor="gray.800" p={1} rounded="sm" mb={5}>
+                            <Option title="Seus palpites" 
+                            isSelected={optionSelected ==='guesses'}
+                            onPress={()=> setOptionSelected('guesses')}/>
+
+                            <Option title="Ranking do grupo" 
+                            isSelected={optionSelected ==='ranking'}
+                            onPress={()=> setOptionSelected('ranking')}/>
+                        </HStack>
+
+                    </VStack>
+
+                    : <EmptyMyPoolList code={poolDetails.code} />
+            }
+
         </VStack>
     );
 }
